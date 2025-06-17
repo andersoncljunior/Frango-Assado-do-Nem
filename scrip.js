5c70fdf6bd668f3a0803aa8eca20b616ae2c7895
andersoncljunior <andersoncljunior@gmail.com>
2025-06-17 14:39:37 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -3,10 +3,10 @@ document.getElementById("pedidoForm").addEventListener("submit", function(event)
 
   const nome = document.getElementById("nome").value;
   const telefone = document.getElementById("telefone").value;
-  const bairro = document.getElementById("bairro").value;
+  const endereco = document.getElementById("endereco").value;
   const pagamento = document.getElementById("pagamento").value;
 
-  const mensagem = `Olá! Meu nome é ${nome}, telefone ${telefone}. Quero 1 frango assado para entregar no bairro ${bairro}. Pagamento: ${pagamento}.`;
+  const mensagem = `Olá! Meu nome é ${nome}, telefone ${telefone}. Quero 1 frango assado para entregar no endereço: ${endereco}. Pagamento: ${pagamento}. Aguardo confirmação!`;
 
   const url = `https://wa.me/5521986164548?text=${encodeURIComponent(mensagem)}`;
   window.open(url, "_blank");
