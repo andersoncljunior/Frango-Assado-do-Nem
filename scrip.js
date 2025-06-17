02468eaee2127237d121703fba0f6613e5538c80
andersoncljunior <andersoncljunior@gmail.com>
2025-06-17 15:05:26 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -8,8 +8,11 @@ document.getElementById("pedidoForm").addEventListener("submit", function(event)
 
   const mensagem = `Olá! Meu nome é ${nome}, telefone ${telefone}. Quero 1 frango assado para entregar no endereço: ${endereco}. Pagamento: ${pagamento}. Aguardo confirmação!`;
 
-  const url =`https://wa.me/5521986164548?text=${encodeURIComponent(mensagem)}`;
-  console.log("Link gerado:", url);
-  window.open(url, "_blank");
+  const url = `https://wa.me/5521986164548?text=${encodeURIComponent(mensagem)}`;
 
+  // Cria um link temporário e simula clique
+  const link = document.createElement("a");
+  link.href = url;
+  link.target = "_blank";
+  link.click();
 });
