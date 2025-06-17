193a6f22edd06aea8e9771c141adefa0f06cfbd9
andersoncljunior <andersoncljunior@gmail.com>
2025-06-17 15:21:45 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,18 +1,16 @@
-document.getElementById("pedidoForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+document.getElementById("pedidoForm").addEventListener("change", atualizarLink);
+document.getElementById("pedidoForm").addEventListener("keyup", atualizarLink);
 
+function atualizarLink() {
   const nome = document.getElementById("nome").value;
   const telefone = document.getElementById("telefone").value;
   const endereco = document.getElementById("endereco").value;
   const pagamento = document.getElementById("pagamento").value;
 
-  const mensagem = `Olá! Meu nome é ${nome}, telefone ${telefone}. Quero 1 frango assado para entregar no endereço: ${endereco}. Pagamento: ${pagamento}. Aguardo confirmação!`;
+  if (nome && telefone && endereco && pagamento) {
+    const mensagem = `Olá! Meu nome é ${nome}, telefone ${telefone}. Quero 1 frango assado para entregar no endereço: ${endereco}. Pagamento: ${pagamento}. Aguardo confirmação!`;
+    const url = `https://wa.me/5521986164548?text=${encodeURIComponent(mensagem)}`;
 
-  const url = `https://wa.me/5521986164548?text=${encodeURIComponent(mensagem)}`;
-
-  // Cria um link temporário e simula clique
-  const link = document.createElement("a");
-  link.href = url;
-  link.target = "_blank";
-  link.click();
-});
+    document.getElementById("linkPedido").href = url;
+  }
+}
